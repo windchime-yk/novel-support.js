76001c653ecf7102d5dff4996333c45fdb182ccb
Tomoya Shijima <evan9274@gmail.com>
2018-04-30 17:55:32 +0900

    リリースに向けた編集

--- docs/main.js
+++ docs/main.js
@@ -6,22 +6,13 @@
 const novelSupport = (elem, option = {}) => {
   // デフォルト引数（一部を上書きすると他がundefinedになるので、条件演算子で別途定義）
   const optionContent = option.content === undefined ? 'text' : option.content;
-  const optionMark = option.mark === undefined ? 'dot' : option.mark;
   const optionRewrite = option.rewrite === undefined ? false : option.rewrite;
   console.log(optionContent, optionMark, optionRewrite);
 
   // 各変数群
   const txtBase = '<p class="ns_indent">$1</p>';
-  const rubyBase = '<ruby>$1<rt>$2</rt></ruby>';
-  // const rubyBase = '<ruby>$1<rt>$2</rt></ruby>';
-  // unicodeで書き直す（参考：https://www.benricho.org/symbol/kigou_01.html, https://www.compart.com/en/unicode/U+FE45）
-  const emphasisList = {
-    'dot': '・', // ・
-    'sesame': '﹅', // ﹅
-  };
-  const emphasisDefine = emphasisList[optionMark];
+  const rubyBase = '<ruby class="ns_ruby">$1<rt>$2</rt></ruby>';
   const emphasisBase = '<strong class="ns_emphasis">$1</strong>';
-  // const emphasisBase = `<ruby>$1<rt>${emphasisDefine}</rt></ruby>`;
   console.log(`現在のnovel-support.jsのコンテンツ変換は${optionContent}です`);
   
   // すべての変換処理
