3b1aa658a4d2a1995753f51fae75dc5c5bd9ac8d
WindChimeYK <windchimeyk@gmail.com>
2018-08-19 14:15:47 +0900

    JSDocの編集

--- src/novel-support.js
+++ src/novel-support.js
@@ -1,7 +1,7 @@
 /**
- * 小説投稿サイトで記述した文章をelemで指定したタグ内に入れることで、小説投稿サイトと同じように表示することができる関数式。
- * @param {string} elem
- * @param {object} option
+ * 小説投稿サイトで記述した文章をelemで指定したタグ内に入れることで、小説投稿サイトと同じように表示することができるパッケージ
+ * @param {string} elem 代替記号の入った文章を入れるタグ
+ * @param {object} option パッケージに指定するオプション（[詳細はREADME参照]{@link https://github.com/windchime-yk/novel-support.js#readme}）
  */
 const novelSupport = (elem, option = {}) => {
   const optionContent = option.content || 'text';
