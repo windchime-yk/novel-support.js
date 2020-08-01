37a674adce546923d00a944e9734abab11943fd1
WhyK <windchimeyk@gmail.com>
2020-08-02 00:30:32 +0900

    update: lyntからESLintに移行

--- src/index.ts
+++ src/index.ts
@@ -7,38 +7,38 @@ interface Option {
   content?: string
 }
 
-const novelSupport = (elem: string, {content = 'text'}: Option = {}) => {
-
+const novelSupport = (elem: string, { content = 'text' }: Option = {}) => {
   const Base = {
     txt: '<p class="ns_indent">$1</p>',
     ruby: '<ruby class="ns_ruby">$1<rt>$2</rt></ruby>',
-    emphasis: '<strong class="ns_emphasis">$1</strong>'
+    emphasis: '<strong class="ns_emphasis">$1</strong>',
   }
   const Reg = {
     txt: /(.+?)[\n\r]/g,
     ruby: /[|｜](.+?)《(.+?)》/g,
-    emphasis: /《《(.+?)》》/g
+    emphasis: /《《(.+?)》》/g,
   }
 
-  const elemList = document.querySelectorAll(elem);
+  const elemList = document.querySelectorAll(elem)
 
   for (const element of elemList) {
-    let obj: string;
-    let replaceBody: string;
+    let obj: string
+    let replaceBody: string
 
     if (content === 'text') {
-      obj = element.textContent;
-      replaceBody = obj.replace(Reg.txt, Base.txt)
-                       .replace(Reg.ruby, Base.ruby)
-                       .replace(Reg.emphasis, Base.emphasis);
-    }
-    else if(content === 'html') {
-      obj = element.innerHTML;
-      replaceBody = obj.replace(Reg.ruby, Base.ruby)
-                       .replace(Reg.emphasis, Base.emphasis);
+      obj = element.textContent
+      replaceBody = obj
+        .replace(Reg.txt, Base.txt)
+        .replace(Reg.ruby, Base.ruby)
+        .replace(Reg.emphasis, Base.emphasis)
+    } else if (content === 'html') {
+      obj = element.innerHTML
+      replaceBody = obj
+        .replace(Reg.ruby, Base.ruby)
+        .replace(Reg.emphasis, Base.emphasis)
     }
 
-    element.innerHTML = replaceBody;
+    element.innerHTML = replaceBody
   }
 }
 
